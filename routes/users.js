ffdedaf0c6f8fe74e3c70993acdd7461793e3b09
Jelani Woods <jelani@firstdraft.com>
2022-10-29 11:45:37 -0500

    This is better for defining methods for multiple routes

--- routes/users.js
+++ routes/users.js
@@ -13,16 +13,15 @@ router.post('/', (req, res) => {
   res.send("User Create")
 })
 
-router.get('/:id', (req, res) => {
-  res.send(`User Get with id: ${req.params.id}`)
-})
-
-router.put('/:id', (req, res) => {
-  res.send(`User Get with id: ${req.params.id}`)
-})
-
-router.delete('/:id', (req, res) => {
-  res.send(`User Get with id: ${req.params.id}`)
-})
+router.route('/:id')
+  .get( (req, res) => {
+    res.send(`User Get with id: ${req.params.id}`)
+  })
+  .put( (req, res) => {
+    res.send(`User Get with id: ${req.params.id}`)
+  })
+  .delete( (req, res) => {
+    res.send(`User Get with id: ${req.params.id}`)
+  })
 
 module.exports = router
