0843c2fc4ad480320c3937411e0f8863262924a6
Jelani Woods <jelani@firstdraft.com>
2022-10-29 11:09:17 -0500

    Can make user download file on visit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.status(200).json({ status: 200, message: "OK" })
+  res.download('server.js')
 })
 
 app.listen(3000);
