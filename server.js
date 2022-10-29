3afee6909addd8a28fe82661d640b74799f93a5b
Jelani Woods <jelani@firstdraft.com>
2022-10-29 11:23:17 -0500

    Prefer locals.propertyName for rendering data
    locals, refers to the actual object argument in the render method (i think)? So it's always defined.
    
    this will prevent 'prop not defined' errors
    
    For learning, it seems more clear to NOT use locals

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Hello, World!'})
+  res.render('index', { message: 'Hello, World!'})
 })
 
 app.listen(3000);
