31eacbe24ffd715f57f700f70007f3a379980b48
Jelani Woods <jelani@firstdraft.com>
2022-10-29 11:41:48 -0500

    Can create dynamic path segments too

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,14 @@ router.get('/new', (req, res) => {
   res.send("User New")
 })
 
+router.post('/', (req, res) => {
+  res.send("User Create")
+})
+
+router.get('/:id', (req, res) => {
+  // params.fetch(:id)
+  req.params.id
+  res.send(`User Get with id: ${req.params.id}`)
+})
+
 module.exports = router
